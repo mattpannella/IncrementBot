99d2eff710d50da64be5b3dc8881de9fa870dced
Matt Pannella <mattpannella@gmail.com>
2023-02-28 22:13:12 -0500

    it kidna works

--- IncrementBot/Program.cs
+++ IncrementBot/Program.cs
@@ -1,183 +1,163 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Discord;
-using Discord.Commands;
 using Discord.WebSocket;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-
-
-
-namespace IncrementBot
+namespace BasicBot
 {
-
-    public class IncrementChannelData
+    // This is a minimal, bare-bones example of using Discord.Net.
+    //
+    // If writing a bot with commands/interactions, we recommend using the Discord.Net.Commands/Discord.Net.Interactions
+    // framework, rather than handling them yourself, like we do in this sample.
+    //
+    // You can find samples of using the command framework:
+    // - Here, under the TextCommandFramework sample
+    // - At the guides: https://discordnet.dev/guides/text_commands/intro.html
+    //
+    // You can find samples of using the interaction framework:
+    // - Here, under the InteractionFramework sample
+    // - At the guides: https://discordnet.dev/guides/int_framework/intro.html
+    class Program
     {
-        public ulong ChannelId { get; set; }
-        public ulong LastUserId { get; set; } = 0;
-        public int CurrentCount { get; set; } = 0;
-        public Dictionary<ulong, int> UserCounts { get; set; } = new Dictionary<ulong, int>();
-    }
+        // Non-static readonly fields can only be assigned in a constructor.
+        // If you want to assign it elsewhere, consider removing the readonly keyword.
+        private readonly DiscordSocketClient _client;
+        private string token = "MTA4MDE5MDUxODIwMzUzNTM4MA.Gm9_oq.ccyhrme_6hLd0XHZ7W74iMn7SNAzZ2-WTd2XN8";
+        private Dictionary<ulong, int> userTotals;
+        private ulong mostRecentUser;
 
-    public class IncrementBot
-    {
-        private DiscordSocketClient _client;
-        private CommandService _commands;
-        private readonly Dictionary<ulong, IncrementChannelData> _incrementChannels = new Dictionary<ulong, IncrementChannelData>();
+        private int count = 0;
 
-        public async Task MainAsync(string token)
+        // Discord.Net heavily utilizes TAP for async, so we create
+        // an asynchronous context from the beginning.
+        static void Main(string[] args)
+            => new Program()
+                .MainAsync()
+                .GetAwaiter()
+                .GetResult();
+
+        public Program()
         {
-            _client = new DiscordSocketClient();
-            _commands = new CommandService();
+            userTotals = new Dictionary<ulong, int>();
 
-            _client.Log += Log;
+            // Config used by DiscordSocketClient
+            // Define intents for the client
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
 
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
+            // It is recommended to Dispose of a client when you are finished
+            // using it, at the end of your app's lifetime.
+            _client = new DiscordSocketClient(config);
 
-            _client.MessageReceived += HandleMessageAsync;
-            _client.ReactionAdded += HandleReactionAsync;
-            _client.Ready += OnReadyAsync;
-
-            await Task.Delay(-1);
+            // Subscribing to client events, so that we may receive them whenever they're invoked.
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+            _client.InteractionCreated += InteractionCreatedAsync;
         }
 
-        private Task OnReadyAsync()
+        public async Task MainAsync()
         {
-            Console.WriteLine($"IncrementBot is connected!");
+            // Tokens should be considered secret data, and never hard-coded.
+            await _client.LoginAsync(TokenType.Bot, token);
+            // Different approaches to making your token a secret is by putting them in local .json, .yaml, .xml or .txt files, then reading them on startup.
+
+            await _client.StartAsync();
+
+            // Block the program until it is closed.
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
             return Task.CompletedTask;
         }
 
-        private async Task HandleMessageAsync(SocketMessage message)
+        // The Ready event indicates that the client has opened a
+        // connection and it is now safe to access the cache.
+        private Task ReadyAsync()
         {
-            // Ignore system messages, other bots, and DMs
-            if (!(message is SocketUserMessage msg) || msg.Author.IsBot || msg.Channel is IDMChannel) return;
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
 
-            // Check if this is an "i!begin" command
-            if (msg.Content.Equals("i!begin") && msg.Author is SocketGuildUser user && user.GuildPermissions.Administrator)
-            {
-                // Check if the channel is already an incrementing channel
-                if (_incrementChannels.ContainsKey(msg.Channel.Id))
-                {
-                    await msg.Channel.SendMessageAsync($"This channel is already an incrementing channel!");
+            return Task.CompletedTask;
+        }
+
+        // This is not the recommended way to write a bot - consider
+        // reading over the Commands Framework sample.
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            // The bot should never respond to itself. dont respond if same user twice in a row
+            if (message.Author.Id == _client.CurrentUser.Id || message.Author.Id == mostRecentUser) {
+                return;
+            }
+
+            //check for leaderboard command. return afterwards
+            if (message.Content == "i!leaderboard") {
+                foreach (ulong key in userTotals.Keys) {
+                    var user = _client.GetUserAsync(key).Result;
+                    var Username = user.Username + "#" + user.Discriminator;
+                    await message.Channel.SendMessageAsync("User " + Username + ": " + userTotals[key]);
                     return;
                 }
+            }
 
-                _incrementChannels.Add(msg.Channel.Id, new IncrementChannelData());
-                await msg.Channel.SendMessageAsync($"This channel is now an incrementing channel!");
-            }
-            else if (_incrementChannels.ContainsKey(msg.Channel.Id) && int.TryParse(msg.Content, out var count) && count == _incrementChannels[msg.Channel.Id].Count + 1)
-            {
-                var data = _incrementChannels[msg.Channel.Id];
-                var userCount = data.UserCounts.GetValueOrDefault(msg.Author.Id);
-                if (userCount >= 1 && data.LastUserId == msg.Author.Id)
-                {
-                    await msg.Channel.SendMessageAsync($"Sorry {msg.Author.Mention}, you can't go twice in a row!");
+            //number will contain the integer from the chat, if its a int
+            int number;
+            bool check = int.TryParse(message.Content, out number);
+            
+            //if it was an int and its previous+1
+            if(check != false && number == (count+1)) {
+                count++;
+                await message.AddReactionAsync(new Emoji("✅"));
+                ulong userid = message.Author.Id;
+                mostRecentUser = message.Author.Id;
+                if(!userTotals.ContainsKey(userid)){
+                    userTotals.Add(userid, 1);
+                } else {
+                    userTotals[userid]++;
                 }
+            } else {
+                //otherwise send the fail  message
+                await message.Channel.SendMessageAsync(GetRandomIncorrectResponse(count));
+                await message.AddReactionAsync(new Emoji("❌"));
+            }
+        }
+
+        // For better functionality & a more developer-friendly approach to handling any kind of interaction, refer to:
+        // https://discordnet.dev/guides/int_framework/intro.html
+        private async Task InteractionCreatedAsync(SocketInteraction interaction)
+        {
+            // safety-casting is the best way to prevent something being cast from being null.
+            // If this check does not pass, it could not be cast to said type.
+            if (interaction is SocketMessageComponent component)
+            {
+                // Check for the ID created in the button mentioned above.
+                if (component.Data.CustomId == "unique-id")
+                    await interaction.RespondAsync("Thank you for clicking my button!");
+
                 else
-                {
-                    if (userCount == 0) data.LastUserId = msg.Author.Id;
-                    data.UserCounts[msg.Author.Id] = userCount + 1;
-                    data.Count = count;
-                    await msg.Channel.SendMessageAsync($"<:check:123456789012345678> {count}");
-                    if (count % 100 == 0)
-                    {
-                        var topUsers = GetTopUsers(data.UserCounts);
-                        await msg.Channel.SendMessageAsync($"Whoa! That's a big number, check out who contributed the most!\n{string.Join("\n", topUsers)}");
-                    }
-                    else if (count % 10 == 0)
-                    {
-                        await msg.Channel.SendMessageAsync($"Nice job {msg.Author.Mention}! Keep it up!");
-                    }
-                }
-            }
-            else if (_incrementChannels.ContainsKey(msg.Channel.Id) && int.TryParse(msg.Content, out var incorrectCount))
-            {
-                var data = _incrementChannels[msg.Channel.Id];
-                await msg.Channel.SendMessageAsync(GetRandomIncorrectResponse(data.Count));
-                await msg.AddReactionAsync(new Emoji("❌"));
+                    Console.WriteLine("An ID has been received that has no handler!");
             }
         }
 
         private string GetRandomIncorrectResponse(int expectedCount)
         {
             string[] incorrectResponses = new string[] {
-        $"Nope, the next number is {expectedCount + 1}",
-        $"Sorry, it's not {expectedCount}, try {expectedCount + 1}",
-        $"That's a good number, but we're looking for {expectedCount + 1}",
-        $"Incorrect! The next number is {expectedCount + 1}",
-        $"Not quite, next number is {expectedCount + 1}",
-        $"Almost there, but not quite {expectedCount + 1}",
-        $"Try again! {expectedCount + 1} is the next number",
-        $"Better luck next time! {expectedCount + 1} is the correct number",
-        $"Nice try, but the next number is {expectedCount + 1}"
+                $"Nope, the next number is {expectedCount + 1}",
+                $"Sorry, it's not {expectedCount}, try {expectedCount + 1}",
+                $"That's a good number, but we're looking for {expectedCount + 1}",
+                $"Incorrect! The next number is {expectedCount + 1}",
+                $"Not quite, next number is {expectedCount + 1}",
+                $"Almost there, but not quite {expectedCount + 1}",
+                $"Try again! {expectedCount + 1} is the next number",
+                $"Better luck next time! {expectedCount + 1} is the correct number",
+                $"Nice try, but the next number is {expectedCount + 1}"
             };
             return incorrectResponses[new Random().Next(incorrectResponses.Length)];
         }
-
-        private async Task HandleReactionAsync(Cacheable<IUserMessage, ulong> messageCacheable, ISocketMessageChannel channel, SocketReaction reaction)
-        {
-            // Check if the reaction is from the bot itself, and if so, ignore it
-            if (reaction.UserId == _client.CurrentUser.Id)
-                return;
-
-            // Retrieve the message that the reaction was added to from the cache
-            var message = await messageCacheable.GetOrDownloadAsync();
-
-            // Check if the message is a valid counting message
-            if (message.Author.IsBot || !_countingChannels.ContainsKey(channel.Id) || !int.TryParse(message.Content, out int count) || count != _currentCount + 1)
-                return;
-
-            // Check if the user is trying to count twice in a row
-            if (message.Author.Id == _lastUserToCount)
-            {
-                await message.AddReactionAsync(new Emoji("❌"));
-                return;
-            }
-
-            // Update the current count and the user's score
-            _currentCount = count;
-            _lastUserToCount = message.Author.Id;
-            if (!_userScores.ContainsKey(message.Author.Id))
-                _userScores[message.Author.Id] = 1;
-            else
-                _userScores[message.Author.Id]++;
-
-            // Add a checkmark reaction to the message
-            await message.AddReactionAsync(new Emoji("✅"));
-
-            // Check if the current count is a multiple of 10 or 100, and display the leaderboard if so
-            if (_currentCount == 10 || _currentCount % 100 == 0)
-            {
-                await DisplayLeaderboard(channel);
-            }
-        }
-
-        private async Task DisplayLeaderboard(ISocketMessageChannel channel)
-        {
-            // Retrieve the top ten users with the highest scores
-            var topUsers = _userScores.OrderByDescending(pair => pair.Value).Take(10);
-
-            // Construct the leaderboard message
-            var leaderboardMessage = new StringBuilder();
-            leaderboardMessage.AppendLine($"Whoa! That's a big number, check out who contributed the most!");
-            leaderboardMessage.AppendLine("```");
-            leaderboardMessage.AppendLine($"{"User",-30} Score");
-            foreach (var userScore in topUsers)
-            {
-                var user = _client.GetUser(userScore.Key);
-                if (user != null)
-                {
-                    leaderboardMessage.AppendLine($"{user.Username,-30} {userScore.Value}");
-                }
-            }
-            leaderboardMessage.AppendLine("```");
-
-            // Send the leaderboard message to the channel
-            await channel.SendMessageAsync(leaderboardMessage.ToString());
-        }
-
     }
 }
